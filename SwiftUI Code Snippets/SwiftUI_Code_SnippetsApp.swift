e324d41e4b1f65df74351c89fbedbf717dff2a5d
Pieter Dopheide <pieter.dopheide@oclc.org>
2024-10-09 13:21:22 +0200

    Initial Commit

---
+++ SwiftUI Code Snippets/SwiftUI_Code_SnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Code_SnippetsApp.swift
+//  SwiftUI Code Snippets
+//
+//  Created by Dopheide,Pieter on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Code_SnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
